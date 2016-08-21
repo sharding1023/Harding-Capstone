e9b7268d2fc7d83c197624720c76d5909204f7ac
sharding1023 <sara.harding23@gmail.com>
2016-08-21 11:58:02 -0400

    Still needs a lot more work

--- Capstone Linear Regression.R
+++ Capstone Linear Regression.R
@@ -1,7 +1,15 @@
 #Multivariable Linear Regression 
 
+#Start by just using pneumonia data
+
+
 model1 <- lm(Prop.Covered.by.Medicare ~ Median.Income.County + Density.County, data = medicare.pneumonia)
 summary(model1)
 
 model2 <- lm(sqrt.Prop.Covered.by.Medicare ~ log.Median.Income.County + log.Density.County, data = medicare.pneumonia)
 summary(model2)
+
+model3 <- lm(log.Average.Medicare.Payments ~ log.Median.Income.County + log.Density.County, data = medicare.pneumonia)
+summary(model3)
+
+#Add more variabes? 
